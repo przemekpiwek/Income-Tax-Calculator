d8f720764c48222169c19c3368e16b8653a61a27
Przemek Piwek <ppiwek@tempo.io>
2023-10-21 08:55:21 -0600

    Introduce React Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { QueryClient, QueryClientProvider } from "react-query";
 import LandingPage from "./pages/LandingPage";
 import { GlobalStyles } from "./styles/GlobalStyles";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-      <GlobalStyles />
-      <LandingPage />
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyles />
+        <LandingPage />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </>
   );
 }
